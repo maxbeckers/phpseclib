1941efae91f7f58fe19c1a9c626049020c749f9e
Maximilian Beckers <Maximilian.Beckers@bspayone.com>
2018-04-13 11:40:56 +0200

    reset

--- phpseclib/bootstrap.php
+++ phpseclib/bootstrap.php
@@ -5,13 +5,12 @@
  * @license http://www.opensource.org/licenses/mit-license.html MIT License
  */
 
-// This is in our case not needed.
-//if (extension_loaded('mbstring')) {
-//    // 2 - MB_OVERLOAD_STRING
-//    if (ini_get('mbstring.func_overload') & 2) {
-//        throw new \UnexpectedValueException(
-//            'Overloading of string functions using mbstring.func_overload ' .
-//            'is not supported by phpseclib.'
-//        );
-//    }
-//}
+if (extension_loaded('mbstring')) {
+    // 2 - MB_OVERLOAD_STRING
+    if (ini_get('mbstring.func_overload') & 2) {
+        throw new \UnexpectedValueException(
+            'Overloading of string functions using mbstring.func_overload ' .
+            'is not supported by phpseclib.'
+        );
+    }
+}
